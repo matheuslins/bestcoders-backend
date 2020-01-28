03bba63f051432ff41ea9f4bee72d7c78abb4723
Matheus Lins <matheus.lins@br.experian.com>
2020-01-27 22:04:40 -0300

    dotenv

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ const cors = require('cors');
 const routes = require('./routes')
 const http = require('http');
 const { setupWebsocket } = require('./websocket');
-const dotenv = require('dotenv').config()
-
 
 const app = express();
 const server = http.Server(app);
 
 setupWebsocket(server);
 
-mongoose.connect(dotenv.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -22,5 +20,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-server.listen(dotenv.PORT || 3000);
+server.listen(process.env.PORT || 3000);
 
